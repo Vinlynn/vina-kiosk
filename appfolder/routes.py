fb7b5875ded5ce5d5f8361f2eaec18db625f7578
Vinlynn <vinalynnlavariaz@gmail.com>
2024-01-04 00:10:44 -0800

    first commit

---
+++ appfolder/routes.py
@@ -0,0 +1,23 @@
+from appfolder import app, mysql
+# from flask import jsonify
+
+from flask import render_template
+
+@app.route('/')
+def index():
+       cursor = mysql.connection.cursor()
+       cursor.execute("SELECT * FROM products")  # Assuming 'products' is your table name
+       products = cursor.fetchall()
+       print(products)
+       cursor.close()
+       return render_template('index.html', products=products)
+   
+# @app.route('/testconnection')
+# def test_connection():
+#     try:
+#         cursor = mysql.connection.cursor()
+#         cursor.execute('SELECT 1')
+#         return jsonify({"status": 'Connected to the database'})
+#     except Exception as e:
+#         return jsonify({"status": "Connection failed", "error": str(e)})
+    
